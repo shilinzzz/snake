9a8d4bfb7fcecabc1cfb891ba2cc3a23fd716dc9
shilinzzz <56763684+shilinzzz@users.noreply.github.com>
2019-11-06 11:20:01 +0100

    add new function: find food

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Created on Fri Oct 25 16:53:37 2019
 """
 
 import pygame
+import random
 grid_width=30
 grid_height=30
 rows = 30
@@ -14,8 +15,10 @@ columns = 30
 x_size = grid_width * columns
 y_size = grid_height * rows
 
-game_data=[[0 for col in range(columns)] for row in range(rows)]
+#food
+white=(255,255,255)
 
+game_data=[[0 for col in range(columns)] for row in range(rows)]
 game_data[2][2]=1
 game_data[2][3]=2
 game_data[2][4]=3
@@ -27,7 +30,7 @@ def getSnakeHead() :
                 new_r=r
                 new_c=c
                 return new_r,new_c
-    
+ 
 def updateData(length_snake):
     for r in range(len(game_data)):
         for c in range(len(game_data[r])):
@@ -63,6 +66,23 @@ def move_snake(direction): #1:up 2:down 3:left 4:right
         game_data[new_r][new_c] = 1   
     return game_data
 
+length_snake=6
+food_x=0
+food_y=0
+def find_food():
+    global length_snake
+    global food_x
+    global food_y
+    new_r,new_c=getSnakeHead()
+    print(new_r,new_c,food_x,food_y)
+    if food_x==new_r and food_y ==new_c:
+        length_snake +=1
+        food_x = random.randrange(0, columns)
+        food_y = random.randrange(0, rows)  
+        print(food_x, food_y)
+        print("the snake is " + str(length_snake))
+    return length_snake
+
 def draw_rect():
     screen.fill((0,0,0))
     snake_color=(255,0,0)
@@ -70,9 +90,9 @@ def draw_rect():
         for c in range(columns):
             if game_data[r][c]>=1:
                 pygame.draw.rect(screen,snake_color,pygame.Rect(c * grid_height,r * grid_width, grid_height, grid_width))
-                        
+    pygame.draw.rect(screen,white,(food_y * grid_height,food_x * grid_width, 30, 30))     
     pygame.display.flip()
- 
+
 pygame.init()
 screen = pygame.display.set_mode((x_size, y_size))
 
@@ -89,8 +109,9 @@ while not done:
     #call the function
     getSnakeHead()
     move_snake(direction)
+    find_food()
     # handle FPS
-    clock.tick(30)          
+    clock.tick(100)          
                 
 # close and exit
 pygame.display.quit()
